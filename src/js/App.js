d1a70c54834ed9c3fd51617aa5881b3cebc34d2a
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-29 16:07:53 +0100

    Clean up folders

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
