d6a287853464f970830b4d3786394751ba408ce0
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-29 23:48:49 +0100

    Add hamburger icon

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import Logo from '../img/logo-white.svg';
+import React, { Component } from "react";
+import Logo from "../img/logo-white.svg";
 
 class Header extends Component {
   render() {
     return (
       <header>
         <div className="logo">
-          <img src={Logo} alt="Eagle Golf Group"/>
+          <img src={Logo} alt="Eagle Golf Group" />
           <h1>Eagle Golf Group</h1>
+        </div>
+        <div className="nav">
+          <i className="material-icons hamburger-menu">menu</i>
+          
         </div>
       </header>
     );
