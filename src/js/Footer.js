6b9b6afa108c9348622d97f5e3778e21382301b6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-30 20:25:46 +0100

    Create getDate() and output year in footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import Logo from '../img/mywebgraphicdesign-logo.svg';
 
 class Footer extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      year: '' 
+    }
+    this.getDate = this.getDate.bind(this);
+  }
+  
+  componentDidMount() {
+    this.getDate();
+  }
+
+  getDate() {
+    const date = new Date();
+    this.setState({ year: date.getFullYear() });
+  }
   
   render() {
+
+    const { year } = this.state;
     return (
       <footer>
         <div className="copyright">
-          <p>Copyright &copy; Eagle Golf Group All rights reserved</p>
+          <p>Copyright &copy; {year} Eagle Golf Group All rights reserved</p>
         </div>
         <div className="credit">
           <p>Designed by 
