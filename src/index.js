6e06e6c855c8a85fca90188fa540ba02d23a97ad
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-30 02:32:20 +0100

    Add navigation bar to header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.scss';
+import './css/_index.scss';
 import App from './js/App';
 import * as serviceWorker from './serviceWorker';
 
--- src/js/Header.js
+++ src/js/Header.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import Logo from "../img/logo-white.svg";
 
 class Header extends Component {
+
   render() {
+
     return (
       <header>
         <div className="logo">
@@ -13,12 +15,12 @@ class Header extends Component {
           <i className="material-icons hamburger-menu">menu</i>
           <nav className="nav-main">
             <ul>
-              <li className="nav-item">Home</li>
-              <li className="nav-item">Our Ethos</li>
-              <li className="nav-item">Events</li>
-              <li className="nav-item">Testimonials</li>
-              <li className="nav-item">Our Partners</li>
-              <li className="nav-item">Contact Us</li>
+              <li>Home</li>
+              <li>Our Ethos</li>
+              <li>Events</li>
+              <li>Testimonials</li>
+              <li>Our Partners</li>
+              <li>Contact Us</li>
             </ul>
           </nav>
         </div>
