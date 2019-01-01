491f7bc3e3afc63835bf6d5159d6eb9774ae6525
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 00:41:52 +0100

    Add contact item contact

--- src/js/Home.js
+++ src/js/Home.js
@@ -171,6 +171,19 @@ class Home extends Component {
             <header className="grid-item section-header contact-item-1">
               <h2>Contact Us</h2>
             </header>
+            <div className="grid-item contact-item-2">
+              <p>If you would like to organize a charity sporting event, to become a sponsor or have any other query, please contact us.</p>
+              <hr/>
+              <h3>Eagle Golf Group</h3>
+              <div className="contact-telephone">
+                <i class="material-icons">phone</i>
+                <span>+44 (0) 7785 500 205</span>
+              </div>
+              <div className="contact-email">
+                <i class="material-icons">alternate_email</i>
+                <span>johnwade@eaglegolfgroup.co.uk</span>
+              </div>
+            </div>
         
           </div>
         </main>
