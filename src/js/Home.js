a12abfc3d89dc5a65a37c7ccfc637014ec485ec5
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-01 12:58:10 +0100

    Add home-logo

--- src/js/Home.js
+++ src/js/Home.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Carousel from "./Carousel";
+import Logo from "../img/logo-white.svg";
+
 
 class Home extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container" id="home">
         <Header />
         <Carousel />
         <div className="hero-text">
@@ -15,6 +17,12 @@ class Home extends Component {
           </h2>
           <button className="btn-primary">Contact Us <i className="material-icons btn-carousel">send</i></button>
         </div>
+        <main>
+          <div className="home-logo">
+           <img src={Logo} alt="Eagle Golf Group logo"/>
+           <h2>Eagle Golf Group</h2>
+          </div>
+        </main>
         <Footer />
       </div>
     );
