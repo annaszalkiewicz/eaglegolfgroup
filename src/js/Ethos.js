d905743539a776b04e712816c4d47b00b1e20961
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 18:56:49 +0100

    Add content to Ethos component

--- src/js/Ethos.js
+++ src/js/Ethos.js
@@ -1,10 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Ethos extends Component {
   render() {
     return (
-      <section id="ethos">Ethos Component</section>
-    )
+      <section id="ethos">
+        <div className="ethos-container">
+          <header className="grid-item ethos-item-1 section-header">
+            <h2>Our Ethos</h2>
+          </header>
+          <div className="grid-item ethos-item-2">
+            <p>
+              Eagle Golf Group remains a consistent leader throughout the past
+              years in golf tourism and awareness of new golf destinations. We
+              maintain the shaping of golf development with the highest of
+              trust, quality and have become trusted by the European PGA.
+            </p>
+            <p>
+              Eagle Golf Group proceeds boldly towards its goals with
+              versatility, creativity and a clear understanding of today’s
+              worldwide golf needs
+            </p>
+            <p>
+              To be always the preferred golf marketing company, and for our
+              staff to feel proud of working for and being affiliated with Eagle
+              Golf Group of Companies.
+            </p>
+            <p>
+              Eagle Golf Group …..”a bird’s eye view on celebrity charity
+              sporting events”
+            </p>
+          </div>
+          <div className="grid-item ethos-item-3 team">
+            <h2>T</h2>
+            <p>Trust</p>
+          </div>
+          <div className="grid-item ethos-item-4 team">
+            <h2>E</h2>
+            <p>Enthusiasm</p>
+          </div>
+          <div className="grid-item ethos-item-5 team">
+            <h2>A</h2>
+            <p>Achievement</p>
+          </div>
+          <div className="grid-item ethos-item-6 team">
+            <h2>M</h2>
+            <p>Motivation</p>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
