56c3f6972f4ba4fd4a73ccdbf074ba00b76c661d
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 17:20:46 +0100

    Create Nav component

---
+++ src/js/Nav.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class Nav extends Component {
+
+  render() {
+
+    const { isOpen } = this.props;
+
+    return (
+      <div>
+        {isOpen && (
+          <nav className="nav-main open">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/ethos">Our Ethos</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/testimonials">Testimonials</NavLink>
+            <NavLink to="/partners">Our Partners</NavLink>
+            <NavLink to="/contact">Contact Us</NavLink>
+          </nav>
+        )}
+        {!isOpen && (
+          <nav className="nav-main">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/ethos">Our Ethos</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/testimonials">Testimonials</NavLink>
+            <NavLink to="/partners">Our Partners</NavLink>
+            <NavLink to="/contact">Contact Us</NavLink>
+          </nav>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Nav;
