9167c3b4e28d9e575adc2b8a88f6cf35d641300c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 01:34:30 +0100

    Create Contact component

---
+++ src/js/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <section id="contact">Contact Component</section>
+    )
+  }
+}
+
+export default Contact;
