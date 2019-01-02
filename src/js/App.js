b66498a7438148892f90ec44adb7507733c6a5f6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 02:06:17 +0100

    Register router and routes

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react';
-import Home from './Home';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import Ethos from "./Ethos";
+import Events from "./Events";
+import Testimonials from "./Testimonials";
+import Partners from "./Partners";
+import Contact from "./Contact";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/"}
+            render={() => <Home />}
+          />
+
+          <Route
+            path={process.env.PUBLIC_URL + "/ethos"}
+            render={() => <Ethos />}
+          />
+
+          <Route
+            path={process.env.PUBLIC_URL + "/events"}
+            render={() => <Events />}
+          />
+
+          <Route
+            path={process.env.PUBLIC_URL + "/testimonials"}
+            render={() => <Testimonials />}
+          />
+
+          <Route
+            path={process.env.PUBLIC_URL + "/partners"}
+            render={() => <Partners />}
+          />
+
+          <Route
+            path={process.env.PUBLIC_URL + "/contact"}
+            render={() => <Contact />}
+          />
       </div>
     );
   }
