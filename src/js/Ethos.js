94327f9be4ea2c77394b63444fb45937d78fdcdf
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 01:27:17 +0100

    Create Ethos component

---
+++ src/js/Ethos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Ethos extends Component {
+  render() {
+    return (
+      <section id="ethos">Ethos Component</section>
+    )
+  }
+}
+
+export default Ethos;
