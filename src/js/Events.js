fe15cac4e89c90c1da77fbf60bd935107061301b
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 01:29:30 +0100

    Create Events component

---
+++ src/js/Events.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Events extends Component {
+  render() {
+    return (
+      <section id="events">Events Component</section>
+    )
+  }
+}
+
+export default Events;
