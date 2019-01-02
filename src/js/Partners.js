e768f357c7caa26c60fe8b02aaff29c7dac3f735
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 01:33:12 +0100

    Create Partners component

---
+++ src/js/Partners.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Partners extends Component {
+  render() {
+    return (
+      <section id="partners">Partners Component</section>
+    )
+  }
+}
+
+export default Partners;
