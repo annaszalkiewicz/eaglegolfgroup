e567299c39b6809df9f4129f72f826870f8442b8
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-02 01:30:53 +0100

    Create Testimonials component

---
+++ src/js/Testimonials.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Testimonials extends Component {
+  render() {
+    return (
+      <section id="testimonials">Testimonials Component</section>
+    )
+  }
+}
+
+export default Testimonials;
