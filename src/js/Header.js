949b1b8c196d13c79837b85543dc18f7050ca40f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-04 21:35:05 +0100

    Fix nav-main colors

--- src/js/Header.js
+++ src/js/Header.js
@@ -14,15 +14,30 @@ class Header extends Component {
   onClickHandler = () => {
     const { isOpen } = this.state;
     this.setState({ isOpen: !isOpen ? true : false });
-  }
+  };
 
   render() {
-
     const { isOpen } = this.state;
 
     return (
       <section>
-        <header className="header-main">
+        <header
+          className="header-main"
+          style={{
+            background:
+              window.location.pathname === "/"
+                ? "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))"
+                : window.location.pathname === "/ethos"
+                ? "#757A2F"
+                : window.location.pathname === "/events"
+                ? "#677E20"
+                : window.location.pathname === "/testimonials"
+                ? "#273f29"
+                : window.location.pathname === "/partners"
+                ? "#14253f"
+                : "#091327"
+          }}
+        >
           <div className="logo">
             <img src={Logo} alt="Eagle Golf Group" />
             <h1>Eagle Golf Group</h1>
