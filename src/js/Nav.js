c109bf38e3dd742fbc64af473009352c33c38713
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-07 15:59:00 +0100

    Remove animation from nav

--- src/js/Nav.js
+++ src/js/Nav.js
@@ -3,42 +3,34 @@ import { NavLink } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 
 class Nav extends Component {
-
   render() {
-
     const { isOpen } = this.props;
 
     return (
       <div>
         {isOpen && (
           <nav className="nav-main open">
-            <Fade top cascade>
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-              <NavLink to="/ethos">Our Ethos</NavLink>
-              <NavLink to="/events">Events</NavLink>
-              <NavLink to="/testimonials">Testimonials</NavLink>
-              <NavLink to="/partners">Our Partners</NavLink>
-              <NavLink to="/contact">Contact Us</NavLink>
-            </Fade>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/ethos">Our Ethos</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/testimonials">Testimonials</NavLink>
+            <NavLink to="/partners">Our Partners</NavLink>
+            <NavLink to="/contact">Contact Us</NavLink>
           </nav>
         )}
         {!isOpen && (
-                      <Fade top duration={2000} cascade>
-
           <nav className="nav-main">
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-              <NavLink to="/ethos">Our Ethos</NavLink>
-              <NavLink to="/events">Events</NavLink>
-              <NavLink to="/testimonials">Testimonials</NavLink>
-              <NavLink to="/partners">Our Partners</NavLink>
-              <NavLink to="/contact">Contact Us</NavLink>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/ethos">Our Ethos</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/testimonials">Testimonials</NavLink>
+            <NavLink to="/partners">Our Partners</NavLink>
+            <NavLink to="/contact">Contact Us</NavLink>
           </nav>
-          </Fade>
-
         )}
       </div>
     );
