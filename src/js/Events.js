632d7f29d30532d779b858c6fe895a58b7bc5e37
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-07 14:47:53 +0100

    Add animations to events page

--- src/js/Events.js
+++ src/js/Events.js
@@ -1,30 +1,75 @@
-import React, { Component } from 'react';
-import Sos from '../img/sos.png';
-import Olympics from '../img/special-olympics.png';
+import React, { Component } from "react";
+import Sos from "../img/sos.png";
+import Olympics from "../img/special-olympics.png";
+import Fade from "react-reveal/Fade";
 
 class Events extends Component {
   render() {
     return (
       <section id="events">
         <div className="events-container">
-          <header className="grid-item events-item-1 section-header">
-          <h2>Events</h2>
-          </header>
-          <div className="grid-item events-item-2">
-            <p>After an amazingly successful charity Golf and Football Tour Event for the former Aston Villa and Celtic club captain and captain of Bulgaria - Stiliyan Petrov in May 2013 Eagle Golf Group are extremely honoured and proud to announce other charity tour event for May 2019. The Management Team of Eagle Golf are currently working hard in partnership with The National Culture and Tourist Board of Albania and together with both Governments for a magnificent charity Football event in Albania. The confirmed dates are 30 May – 02 June 2019. The event has been already been endorsed and supported by both British and Albanian Ambassadors Government dignitaries.</p>
-            <p>Furthermore we have had an endorsement of approval and support from The Football Federation of Albania. The core purpose and goal of this charity event is to raise funds for The SOS orphanage Children of Albania and The Special Olympics Albania. We also envisage to invite the global mega super star to join in this magnificent charity event to sing in the national stadium.</p>
-            <p>This magnificent good cause charity football event will be schedule around the opening of the new and spectacular national football stadium recently been endorsed by FIFA.</p>
-            <p>The charity football match will be against Former Albanian Legends X1 v Former England Legends X1.</p>
-            <p>We are also organising a magnificent Gala Dinner Presentation for the Albanian event which we are honoured will be hosted by The Mayor and Municipality of Tirana.</p>
-            <p>Further information how to book and join these unique charity events and meet your football heroes will be announced in March 2019.</p>
-          </div>
-          <div className="grid-item events-item-3">
-            <img src={Sos} alt="SOS Children's Villages" className="sos"/>
-            <img src={Olympics} alt="Special Olympics Albania" className="olympics"/>
-          </div>
+          <Fade left>
+            <header className="grid-item events-item-1 section-header">
+              <h2>Events</h2>
+            </header>
+          </Fade>
+          <Fade right>
+            <div className="grid-item events-item-2">
+              <p>
+                After an amazingly successful charity Golf and Football Tour
+                Event for the former Aston Villa and Celtic club captain and
+                captain of Bulgaria - Stiliyan Petrov in May 2013 Eagle Golf
+                Group are extremely honoured and proud to announce other charity
+                tour event for May 2019. The Management Team of Eagle Golf are
+                currently working hard in partnership with The National Culture
+                and Tourist Board of Albania and together with both Governments
+                for a magnificent charity Football event in Albania. The
+                confirmed dates are 30 May – 02 June 2019. The event has been
+                already been endorsed and supported by both British and Albanian
+                Ambassadors Government dignitaries.
+              </p>
+              <p>
+                Furthermore we have had an endorsement of approval and support
+                from The Football Federation of Albania. The core purpose and
+                goal of this charity event is to raise funds for The SOS
+                orphanage Children of Albania and The Special Olympics Albania.
+                We also envisage to invite the global mega super star to join in
+                this magnificent charity event to sing in the national stadium.
+              </p>
+              <p>
+                This magnificent good cause charity football event will be
+                schedule around the opening of the new and spectacular national
+                football stadium recently been endorsed by FIFA.
+              </p>
+              <p>
+                The charity football match will be against Former Albanian
+                Legends X1 v Former England Legends X1.
+              </p>
+              <p>
+                We are also organising a magnificent Gala Dinner Presentation
+                for the Albanian event which we are honoured will be hosted by
+                The Mayor and Municipality of Tirana.
+              </p>
+              <p>
+                Further information how to book and join these unique charity
+                events and meet your football heroes will be announced in March
+                2019.
+              </p>
+            </div>
+          </Fade>
+          <Fade left delay={500}>
+            <div className="grid-item events-item-3">
+              <img src={Sos} alt="SOS Children's Villages" className="sos" />
+              <img
+                src={Olympics}
+                alt="Special Olympics Albania"
+                className="olympics"
+              />
+            </div>
+          </Fade>
         </div>
       </section>
-    )
+    );
   }
 }
 
