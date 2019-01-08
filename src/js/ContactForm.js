52ffcbd42097c25a6786a5256d19dbbd37111d1f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-08 16:13:52 +0100

    Change port

--- src/js/ContactForm.js
+++ src/js/ContactForm.js
@@ -17,7 +17,7 @@ class ContactForm extends Component {
     const message = document.getElementById("message").value;
     axios({
       method: "POST",
-      url: "http://localhost:3002/send",
+      url: "http://localhost:3000/send",
       data: {
         firstName: firstName,
         lastName: lastName,
