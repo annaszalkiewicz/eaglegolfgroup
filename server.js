ae613325c2eaf9ea6aa3823e24f782d13a5f5dd8
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-09 23:01:11 +0100

    Change email address that email is sent to

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post("/send", (req, res, next) => {
 
   var mail = {
     from: `${email}`,
-    to: "info@eaglegolfgroup.co.uk", //Change to email address that you want to receive messages on
+    to: "johnwade@eaglegolfgroup.co.uk", //Change to email address that you want to receive messages on
     subject: "New Message from Contact Form",
     text: `${content}`
   };
