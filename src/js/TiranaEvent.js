98fe5e6c788a8408611f436964580bf7d832f1fd
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-02-01 23:18:15 +0100

    Create TiranaEvent component

---
+++ src/js/TiranaEvent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TiranaEvent extends Component {
+
+  render() {
+    return (
+      <main id="tirana-event"></main>
+    )
+  }
+}
+
+export default TiranaEvent;
