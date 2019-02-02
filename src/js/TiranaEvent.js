266bf82ce9c79af27c905db54bffc980f7ac160b
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-02-02 23:56:34 +0100

    Add tab list

--- src/js/TiranaEvent.js
+++ src/js/TiranaEvent.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class TiranaEvent extends Component {
 
   render() {
     return (
-      <main id="tirana-event"></main>
+      <main id="tirana-event">
+        <Tabs>
+
+          <TabList>
+            <Tab>About</Tab>
+            <Tab>Program</Tab>
+            <Tab>Sponsors</Tab>
+            <Tab>Endorsements</Tab>
+          </TabList>
+
+
+        </Tabs>
+      </main>
     )
   }
 }
